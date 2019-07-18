be545b228e1b55d8f5ebf7d27d37fbbeec4b0fa8
DonLakeFlyer <dongagne@outlook.com>
2019-07-18 14:51:19 -0700

    Send frequency in udp pulse data

--- python/udp_sender_f.py
+++ python/udp_sender_f.py
@@ -64,7 +64,14 @@ class udp_sender_f(gr.sync_block):
                 if self.cpuTemp:
                     temp = self.cpuTemp.temperature
                 try:
-                    self.socket.sendto(struct.pack('<iffi', self.channelIndex, pulseValue, temp, self.pulseDetectBase.get_gain()), self.sendAddress)
+                    self.socket.sendto(
+                        struct.pack('<iffii', 
+                            self.channelIndex, 
+                            pulseValue, 
+                            temp, 
+                            self.pulseDetectBase.get_pulse_freq(),
+                            self.pulseDetectBase.get_gain()), 
+                        self.sendAddress)
                 except Exception as e:
                     print("Exception udp_sender:work valid pulse send", e)
                 self.lastPulseTime = time.time()
@@ -73,7 +80,14 @@ class udp_sender_f(gr.sync_block):
                 if self.cpuTemp:
                     temp = self.cpuTemp.temperature
                 try:
-                    self.socket.sendto(struct.pack('<iffi', self.channelIndex, 0, temp, self.pulseDetectBase.get_gain()), self.sendAddress)
+                    self.socket.sendto(
+                        struct.pack('<iffii', 
+                            self.channelIndex, 
+                            0, # pulse value
+                            temp, 
+                            self.pulseDetectBase.get_pulse_freq(),
+                            self.pulseDetectBase.get_gain()), 
+                        self.sendAddress)
                 except Exception as e:
                     print("Exception udp_sender:work no pulse send", e)
                 self.lastPulseTime = time.time()
