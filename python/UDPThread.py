bc55025f722e280f3075d754065dd9af99e37a60
DonLakeFlyer <dongagne@outlook.com>
2019-09-16 14:59:26 -0700

    Threaded UDP

---
+++ python/UDPThread.py
@@ -0,0 +1,98 @@
+import threading
+import math
+import logging
+import subprocess
+import socket
+import select
+import struct
+
+try:
+    from gpiozero import CPUTemperature
+except:
+    gpiozerioAvailable = False
+else:
+    gpiozerioAvailable = True
+
+class UDPThread (threading.Thread):
+	exitFlag = False
+
+	def __init__(self, drone, pulseQueue):
+		threading.Thread.__init__(self)
+
+		self.pulseQueue = pulseQueue
+		if gpiozerioAvailable:
+			self.cpuTemp = CPUTemperature()
+		else:
+			self.cpuTemp = None
+
+		self.udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+		if drone:
+			# PDC Drone
+			self.udpSocket.bind(('localhost', 10001))
+			self.sendAddress = ('localhost', 10000) 
+		else:
+			# STE Tracker
+
+			self.udpSocket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 0)
+			self.sendAddress = ('224.0.0.1', 5007) 
+
+			self.tcpSocketServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			self.tcpSocketServer.setblocking(0)
+			self.tcpAddress = ('127.0.0.1', 5005)
+			self.tcpSocketServer.bind(self.tcpAddress)
+			self.tcpSocketServer.listen(1)
+			self.tcpClient = None
+
+			self.udpSocket.setblocking(False)
+
+	def run(self):
+		while True:
+			pulseValue = self.pulseQueue.get(True)
+			print("UDPThread pulseValue", pulseValue)
+
+	def foo(self):
+		# First see if we have a tcp connection
+		#inputs = [ self.tcpSocketServer ]
+		#outputs = []
+		#readable, writable, exceptional = select.select(inputs, outputs, inputs, 0)
+		#for s in readable:
+		#    if s is server:
+		#        print("TCP client accepting", client_address)
+		#        self.tcpClient, client_address = s.accept()
+
+		# Check for incoming commands
+		if self.tcpClient:
+			try:
+				data = self.tcpClient.recv(1024)
+			except:
+				pass
+			else:
+				self.parseCommand(data)
+
+		if math.isnan(pulseValue):
+			return
+
+		temp = 0
+		if self.cpuTemp:
+			temp = self.cpuTemp.temperature
+
+		packedData = struct.pack('<iiffii', 
+						self.sendIndex,
+						self.channelIndex, 
+						pulseValue, 
+						temp, 
+						self.pulseDetectBase.get_pulse_freq(),
+						self.pulseDetectBase.get_gain())
+		try:
+			self.udpSocket.sendto(packedData, self.sendAddress)
+			self.udpSocket.sendto(packedData, self.sendAddress)
+		except Exception as e:
+			print("Exception udp_sender:work Sending pulse to UDP socket", e)
+		if self.tcpClient:
+			try:
+				self.tcpClient.sendall(packedData)
+			except Exception as e:
+				print("Exception udp_sender:work Sending pulse to TCP socket", e)
+		self.sendIndex = self.sendIndex + 1
+
--- python/udp_sender_f.py
+++ python/udp_sender_f.py
@@ -1,20 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import socket
-import select
-import struct
 import math
 import numpy
 import time
 from gnuradio import gr
-
-try:
-    from gpiozero import CPUTemperature
-except:
-    gpiozerioAvailable = False
-else:
-    gpiozerioAvailable = True
+from multiprocessing import Queue
+import UDPThread
 
 class udp_sender_f(gr.sync_block):
     """
@@ -23,72 +15,21 @@ class udp_sender_f(gr.sync_block):
     def __init__(self, channel_index, localhost):
         gr.sync_block.__init__(self, name="udp_sender_f", in_sig=[numpy.float32], out_sig=None)
 
+        self.pulseQueue = Queue()
+
+        self.udpThread = UDPThread.UDPThread(localhost == 1, self.pulseQueue)
+        self.udpThread.start()
+
         self.channelIndex = channel_index
         self.sendIndex = 0
-
-        if gpiozerioAvailable:
-            self.cpuTemp = CPUTemperature()
-        else:
-            self.cpuTemp = None
-
-        self.udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-        if localhost == 1:
-            # VHF Drone
-            self.udpSocket.bind(('localhost', 10001))
-            self.sendAddress = ('localhost', 10000) 
-        else:
-            # STE Tracker
-
-            self.udpSocket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 0)
-            self.sendAddress = ('224.0.0.1', 5007) 
-
-            self.tcpSocketServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.tcpSocketServer.setblocking(0)
-            self.tcpAddress = ('127.0.0.1', 5005)
-            self.tcpSocketServer.bind(self.tcpAddress)
-            self.tcpSocketServer.listen(1)
-            self.tcpClient = None
-
-        self.udpSocket.setblocking(False)
 
         self.pulseDetectBase = None
         self.lastPulseTime = time.time()
 
         self.printOnce = True
-        self.localhost = localhost
 
     def work(self, input_items, output_items):
-        if self.printOnce:
-            print (self.sendAddress, self.localhost)
-            self.printOnce = False
-
         for pulseValue in input_items[0]:
-            # First see if we have a tcp connection
-            inputs = [ self.tcpSocketServer ]
-            outputs = []
-            readable, writable, exceptional = select.select(inputs, outputs, inputs, 0)
-            for s in readable:
-                if s is server:
-                    print("TCP client accepting", client_address)
-                    self.tcpClient, client_address = s.accept()
-
-            # Check for incoming commands
-            if self.tcpClient:
-                try:
-                    data = self.tcpClient.recv(1024)
-                except:
-                    pass
-                else:
-                    self.parseCommand(data)
-
-            if math.isnan(pulseValue):
-                continue
-
-            temp = 0
-            if self.cpuTemp:
-                temp = self.cpuTemp.temperature
-
             sendPulse = False
             if pulseValue > 0:
                 sendPulse = True
@@ -96,24 +37,9 @@ class udp_sender_f(gr.sync_block):
                 sendPulse = True
 
             if sendPulse:
-                packedData = struct.pack('<iiffii', 
-                                self.sendIndex,
-                                self.channelIndex, 
-                                pulseValue, 
-                                temp, 
-                                self.pulseDetectBase.get_pulse_freq(),
-                                self.pulseDetectBase.get_gain())
-                try:
-                    self.udpSocket.sendto(packedData, self.sendAddress)
-                except Exception as e:
-                    print("Exception udp_sender:work Sending pulse to UDP socket", e)
-                if self.tcpClient:
-                    try:
-                        self.tcpClient.sendall(packedData)
-                    except Exception as e:
-                        print("Exception udp_sender:work Sending pulse to TCP socket", e)
+                print("Adding to queue")
+                self.pulseQueue.put(pulseValue)
                 self.lastPulseTime = time.time()
-                self.sendIndex = self.sendIndex + 1
 
         return len(input_items[0])
 
